232b27626aa87c6b24d7eee44d4292ea79b2a978
Mateusz "Mattu" Partyka <mateusz.pl671@gmail.com>
2024-08-08 14:05:27 +0200

    Update Program.cs

--- API/CarManageSystem.API/Program.cs
+++ API/CarManageSystem.API/Program.cs
@@ -4,7 +4,7 @@ using CarManageSystem.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:40076", "https://0.0.0.0:40076");
+builder.WebHost.UseUrls("http://mattu.bieda.it:40076", "https://mattu.bieda.it:40076");
 // Add services to the container.
 
 builder.Services.AddEndpointsApiExplorer();
